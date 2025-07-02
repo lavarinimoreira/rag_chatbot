d182260bd6f054c50554cda32660d07babbfbcd6
lavarinimoreira <lavarinimoreira@gmail.com>
2025-07-02 14:25:31 -0300

    Add RAG-based document processing and vector store setup for chatbot integration

--- app.py
+++ app.py
@@ -1,8 +1,67 @@
+import os
+import tempfile
+
 import streamlit as st
 
+from decouple import config
+
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+
+os.environ["OPENAI_API_KEY"] = str(config("OPENAI_API_KEY"))
+persist_directory = "db"
+
+
+def process_pdf(file):
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_file:
+        temp_file.write(file.read())
+        temp_file_path = temp_file.name
+
+    loader = PyPDFLoader(temp_file_path)
+    docs = loader.load()
+
+    os.remove(temp_file_path)
+
+    text_spliter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=400,
+    )
+    chunks = text_spliter.split_documents(documents=docs)
+    return chunks
+
+
+def load_existing_vector_store():
+    if os.path.exists(os.path.join(persist_directory)):
+        vector_store = Chroma(
+            persist_directory=persist_directory,
+            embedding_function=OpenAIEmbeddings(),
+        )
+        return vector_store
+    return None
+
+
+def add_to_vector_store(chunks, vector_store=None):
+    if vector_store:
+        vector_store.add_documents(chunks)
+    else:
+        vector_store = Chroma.from_documents(
+            documents=chunks,
+            embedding=OpenAIEmbeddings(),
+            persist_directory=persist_directory,
+        )
+    return vector_store
+
+
+vector_store = load_existing_vector_store()
 
 st.set_page_config(page_title="Chat Gotalk", page_icon="🗨️")
 st.header("Chat com seus documentos (RAG)")
+
 
 # Navegação lateral para upload de arquivos
 with st.sidebar:
@@ -12,6 +71,17 @@ with st.sidebar:
         type=["pdf"],
         accept_multiple_files=True,
     )
+
+    if uploaded_files:
+        with st.spinner("Processando documentos..."):
+            all_chunks = []
+            for uploaded_file in uploaded_files:
+                chunks = process_pdf(file=uploaded_file)
+                all_chunks.extend(chunks)
+            vector_store = add_to_vector_store(
+                chunks=all_chunks,
+                vector_store=vector_store,
+            )
 
     # Modelos disponíveis
     model_options = [
